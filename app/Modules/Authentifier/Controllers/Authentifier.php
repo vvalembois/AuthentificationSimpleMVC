cb33d1f1045ca67612543cff3def4b2d90da0d54
Deloffre Alexandre <alexandre_deloffre@ens.univ-artois.fr>
2016-02-21 20:37:01 +0100

    Implémentation du constructeur à terminer plus tard(après qu'on ai défini les différents champs de COOKIE et SESSION que l'on utilisera, Ajout de fonctions utiles qu'il faudra placer dans une classe Helper interne au module

--- app/Modules/Authentifier/Controllers/Authentifier.php
+++ app/Modules/Authentifier/Controllers/Authentifier.php
@@ -4,10 +4,23 @@ namespace Modules\Authentifier\Controllers;
 use Core\Controller;
 use Core\View;
 use Core\Router;
+use Helpers\Request;
+use Helpers\Session;
 
 class Authentifier extends Controller{
 	public function __construct(){
 		parent::__construct();
+		// Initialisation d'une session
+		Session::init();
+
+		// Tester si une session 'concurrente' existe, auquel cas on deconnecte l'utilisateur
+		Authentifier::checkSessionConcurrency();
+
+		// Tester si l'utilisateur est non connecté et a un cookie "Rester connecté"
+		if(!Authentifier::userIsLoggedIn() && /*TODO teste le cookie "Rester connecté"*/){
+			// TODO renvoyer vers la route loginWithCookie
+		}
+
 	}
 	
 	public function routes(){
@@ -16,5 +29,28 @@ class Authentifier extends Controller{
 
 	public function posts(){
 		echo 'authentifier fonctionne';
+	}
+
+	/* TODO il doit être possible de mettre les fonctions utiles ci-dessous dans un helper interne au module*/
+
+	private static function checkSessionConcurrency(){
+		// TODO tester dans la base de donnée l'id de session de l'utilisateur
+	}
+
+	private static function userIsLoggedIn()
+	{
+		// TODO tester la variable de session concernée
+	}
+
+	/**
+	 * Retourne la valeur d'un champ souhaité d'un cookie
+	 * @param mixed $key La clé du champ souhaité
+	 * @return mixed La valeur du champ souhaité ou rien (si inexistant)
+	 */
+	public static function cookie($key)
+	{
+		if (isset($_COOKIE[$key]))
+			return $_COOKIE[$key];
+        return null;
 	}
 }
