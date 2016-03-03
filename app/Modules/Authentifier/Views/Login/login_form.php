487b65c0287c0f85c8ce09e30fb741bf0a45749e
Deloffre Alexandre <alexandre_deloffre@ens.univ-artois.fr>
2016-03-03 11:14:59 +0100

    Création d'un formulaire de login simpliste pour les tests

---
+++ app/Modules/Authentifier/Views/Login/login_form.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * User: deloffre
+ * Date: 02/03/16
+ * Time: 23:02
+ */
+?>
+<form action="#">
+    <input type="text" name="login_name" placeholder="Type your login"/>
+    <input type="password" name="login_password" placeholder="Type your password"/>
+    <input type="submit">
+</form>
