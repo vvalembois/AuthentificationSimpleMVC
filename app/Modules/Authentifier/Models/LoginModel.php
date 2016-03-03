353c0371641bb192ee232e68365930cd8f21f0dd
Deloffre Alexandre <alexandre_deloffre@ens.univ-artois.fr>
2016-03-03 12:17:37 +0100

    Vérification de la variable de session afin de connaitre le statut de connexion de l'utilisateur

--- app/Modules/Authentifier/Models/LoginModel.php
+++ app/Modules/Authentifier/Models/LoginModel.php
@@ -9,8 +9,12 @@ namespace Modules\Authentifier\Models;
 
 
 use Core\Model;
+use Helpers\Session;
 
 class LoginModel extends Model
 {
-
+    public static function userIsLoggedIn()
+    {
+        return Session::get('user_logged_in');
+    }
 }
