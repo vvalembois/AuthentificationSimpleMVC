5790b40f55104a5bac793b71376a112176fc88a1
Elsaesser Quentin <quentin_elsaesser@ens.univ-artois.fr>
2016-03-08 10:58:40 +0100

    Update register_form.php

--- app/Modules/Authentifier/Views/Register/register_form.php
+++ app/Modules/Authentifier/Views/Register/register_form.php
@@ -1,25 +1,25 @@
 <form action="/authentifier/registerAction" method="post">
-    <label for="nom">Nom :</label>
+    <label for="user_name">Nom :</label>
     <input type="text" id="user_name" name="user_name"/>
 
-    <label for="courriel">Adresse mail :</label>
+    <label for="user_mail">Adresse mail :</label>
     <input type="email" id="user_mail" name="user_mail"/>
 
-    <label for="courriel">Répétez votre adresse mail :</label>
+    <label for="user_mail_repeat">Répétez votre adresse mail :</label>
     <input type="email" id="user_mail_repeat" name="user_mail_repeat"/>
 
-    <label for="courriel">Mot de passe :</label>
+    <label for="user_password">Mot de passe :</label>
     <input type="password" id="user_password" name="user_password"/>
 
-    <label for="courriel">Répétez votre mot de passe :</label>
+    <label for="user_password_repeat">Répétez votre mot de passe :</label>
     <input type="password" id="user_password_repeat" name="user_password_repeat"/>
 
-    <label for="courriel">Recopiez le texte de l'image :</label>
+    <label for="user_captcha">Recopiez le texte de l'image :</label>
     <img src= <?= $data['captcha_url'] ?>>
     <input type="text" id="user_captcha" name="user_captcha"/>
 
     <div class="button">
-        <button type="submit">Envoyer </button>
+        <button type="submit">Envoyer</button>
     </div>
 
 </form>
