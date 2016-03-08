439bf3804917848106bb2fa05e4545ac8c1616b3
Valembois Vincent <vincent_valembois@ens.univ-artois.fr>
2016-03-08 11:05:58 +0100

    Update <br/>

--- app/Modules/Authentifier/Controllers/Register.php
+++ app/Modules/Authentifier/Controllers/Register.php
@@ -35,17 +35,17 @@ class Register extends Authentifier
      */
     public function registerAction(){
         $user_name = strip_tags(Request::post('user_name'));
-        echo "<br>".$user_name."<br>";
+        echo "<br>".$user_name."<br/>";
         $user_email = strip_tags(Request::post('user_mail'));
-        echo $user_email."<br>";
+        echo $user_email."<br/>";
         $user_email_repeat = strip_tags(Request::post('user_mail_repeat'));
-        echo $user_email_repeat."<br>";
+        echo $user_email_repeat."<br/>";
         $user_password = strip_tags(Request::post('user_password'));
-        echo $user_password."<br>";
+        echo $user_password."<br/>";
         $user_password_repeat = strip_tags(Request::post('user_password_repeat'));
-        echo $user_password_repeat."<br>";
+        echo $user_password_repeat."<br/>";
         $user_captcha = strip_tags(Request::post('user_captcha'));
-        echo $user_captcha."<br>";
+        echo $user_captcha."<br/>";
 
         echo $user_captcha."<br/>".PHP_EOL;
         $register_successful = RegisterModel::newUser($user_captcha,$user_name,$user_email,$user_email_repeat,$user_password,$user_password_repeat);
