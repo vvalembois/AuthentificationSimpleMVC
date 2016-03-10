19f5372923ebe9db36c7a784dfc3d29ceab82513
Valembois Vincent <vincent_valembois@ens.univ-artois.fr>
2016-03-10 16:09:25 +0100

    Update profil.php

--- app/Modules/Authentifier/Controllers/Profile.php
+++ app/Modules/Authentifier/Controllers/Profile.php
@@ -11,4 +11,32 @@ namespace Modules\Authentifier\Controllers;
 class Profile extends Authentifier
 {
 
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    
+    
+    public function routes()
+    {
+        Router::any('authentifier/profileForm', 'Modules\Authentifier\Controllers\Profiler@profileForm');
+        Router::any('authentifier/profileAction', 'Modules\Authentifier\Controllers\Profile@profileAction');
+    }
+    
+    public function profileForm()
+    {
+        View::renderTemplate('header');
+        $this->feedback->render();
+        View::renderModule('/Authentifier/Views//', $data);
+        View::renderTemplate('footer');
+    }
+    
+    public function profileAction()
+    {
+        View::renderTemplate('header');
+        $this->feedback->render();
+        View::renderModule('/Authentifier/Views//', $data);
+        View::renderTemplate('footer');
+    }
+
 }
