d8e645f18df3f8fc095033195e65e9cd913a7f62
Jerome <jerome_martinsmosca@ens.univ-artois.fr>
2016-03-25 20:13:25 +0100

    ajout des templates pour les mails d'activation de  compte, de mot de passe oublier, de connexion successive échouées, de confirmation

---
+++ app/Modules/Authentifier/Views/Profile/profil_forget_password_mail.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jerome
+ * Date: 25/03/16
+ * Time: 19:51
+ */
+?>
+
+<h1>Forgot password</h1>
+
+<p>You have forgot your password ? you must to visit this link to set up a new password for your account <?php $data['forgot_password_link']?> if it is not you who made this request thank you ignore this mail</p>
---
+++ app/Modules/Authentifier/Views/Register/register_activation_mail.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jerome
+ * Date: 25/03/16
+ * Time: 19:27
+ */
+?>
+<h1>Activate you account</h1>
+
+<p>Welcome to <?php $data['site_name'] ?> <?php$data['user_name'] ?>! for visit the website, You need to visit this url for validate your account <?php $data['confirmation_mail'] ?></p>
