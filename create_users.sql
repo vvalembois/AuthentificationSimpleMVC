2d441ea810433287f3526853f0830183a39477df
Deloffre Alexandre <alexandre_deloffre@ens.univ-artois.fr>
2016-03-27 20:18:24 +0200

    Modification du fichier sql

--- create_users.sql
+++ create_users.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Sam 19 Mars 2016 à 17:43
+-- Généré le :  Dim 27 Mars 2016 à 20:06
 -- Version du serveur :  5.6.28-0ubuntu0.15.10.1
 -- Version de PHP :  5.6.11-1ubuntu3.1
 
@@ -19,6 +19,7 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `authentifier`
 --
+DROP DATABASE `authentifier`;
 CREATE DATABASE IF NOT EXISTS `authentifier` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `authentifier`;
 
@@ -40,25 +41,17 @@ CREATE TABLE `users` (
   `user_account_type` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'user''s account type (basic, premium, etc)',
   `user_has_avatar` tinyint(1) NOT NULL DEFAULT '0' COMMENT '1 if user has a local avatar, 0 if not',
   `user_remember_me_token` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'user''s remember-me cookie token',
-  `user_creation_timestamp` bigint(20) DEFAULT NULL COMMENT 'timestamp of the creation of user''s account',
+  `user_creation_timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'timestamp of the creation of user''s account',
   `user_suspension_timestamp` bigint(20) DEFAULT NULL COMMENT 'Timestamp till the end of a user suspension',
   `user_last_login_timestamp` bigint(20) DEFAULT NULL COMMENT 'timestamp of user''s last login',
   `user_failed_logins` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'user''s failed login attempts',
   `user_last_failed_login` int(10) DEFAULT NULL COMMENT 'unix timestamp of last failed login attempt',
+  `user_last_failed_login_ip` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
   `user_activation_hash` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'user''s email verification hash string',
   `user_password_reset_hash` char(40) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'user''s password reset code',
   `user_password_reset_timestamp` bigint(20) DEFAULT NULL COMMENT 'timestamp of the password reset request',
   `user_provider_type` text COLLATE utf8_unicode_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='user data';
-
---
--- Contenu de la table `users`
--- Les mots de passes des utilisateurs créé par défaut sont 'password'
---
-
-INSERT INTO `users` (`user_id`, `session_id`, `user_name`, `user_password_hash`, `user_email`, `user_active`, `user_deleted`, `user_account_type`, `user_has_avatar`, `user_remember_me_token`, `user_creation_timestamp`, `user_suspension_timestamp`, `user_last_login_timestamp`, `user_failed_logins`, `user_last_failed_login`, `user_activation_hash`, `user_password_reset_hash`, `user_password_reset_timestamp`, `user_provider_type`) VALUES
-(3, NULL, 'administrator', '$2y$10$iYYZiXhBe6x3NChnyDz9IuasAoMYb62dOyUJYyLpwtVqN.tVXNQqi', 'administrator@auth.auth', 0, 0, 2, 0, NULL, NULL, NULL, NULL, 0, NULL, NULL, NULL, NULL, NULL),
-(4, NULL, 'user', '$2y$10$5zo6EbzDcDo8pmYzNVrLMeUUoylijloxm6fBC2ftq62NKDYsbf6oy', 'user@auth.auth', 0, 0, 0, 0, NULL, NULL, NULL, NULL, 0, NULL, NULL, NULL, NULL, NULL);
 
 --
 -- Index pour les tables exportées
@@ -80,7 +73,16 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'auto incrementing user_id of each user, unique index', AUTO_INCREMENT=5;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'auto incrementing user_id of each user, unique index';
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+--
+-- Contenu de la table `users`
+--
+
+INSERT INTO `users` (`session_id`, `user_name`, `user_password_hash`, `user_email`, `user_active`, `user_deleted`, `user_account_type`, `user_has_avatar`, `user_remember_me_token`, `user_creation_timestamp`, `user_suspension_timestamp`, `user_last_login_timestamp`, `user_failed_logins`, `user_last_failed_login`, `user_last_failed_login_ip`, `user_activation_hash`, `user_password_reset_hash`, `user_password_reset_timestamp`, `user_provider_type`) VALUES
+(NULL, 'administrator', '$2y$10$iYYZiXhBe6x3NChnyDz9IuasAoMYb62dOyUJYyLpwtVqN.tVXNQqi', 'administrator@auth.auth', 0, 0, 2, 0, NULL, NULL, NULL, NULL, 3, NULL, NULL, NULL, NULL, NULL, NULL),
+(NULL, 'user', '$2y$10$5zo6EbzDcDo8pmYzNVrLMeUUoylijloxm6fBC2ftq62NKDYsbf6oy', 'user@auth.auth', 0, 0, 0, 0, NULL, NULL, NULL, NULL, 0, NULL, NULL, NULL, NULL, NULL, NULL);
+
