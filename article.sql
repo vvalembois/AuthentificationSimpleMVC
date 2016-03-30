e8894be87d7a03910254450308f00453512ab124
Jerome <jerome_martinsmosca@ens.univ-artois.fr>
2016-03-30 15:19:00 +0200

    modification table article

--- article.sql
+++ article.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Mer 30 Mars 2016 à 14:54
+-- Généré le :  Mer 30 Mars 2016 à 15:17
 -- Version du serveur :  5.6.28-0ubuntu0.15.10.1
 -- Version de PHP :  5.6.11-1ubuntu3.1
 
@@ -45,7 +45,8 @@ CREATE TABLE `article` (
 -- Index pour la table `article`
 --
 ALTER TABLE `article`
-  ADD PRIMARY KEY (`art_id`);
+  ADD PRIMARY KEY (`art_id`),
+  ADD KEY `art_author` (`art_author`);
 
 --
 -- AUTO_INCREMENT pour les tables exportées
@@ -56,6 +57,16 @@ ALTER TABLE `article`
 --
 ALTER TABLE `article`
   MODIFY `art_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `article`
+--
+ALTER TABLE `article`
+  ADD CONSTRAINT `article_ibfk_1` FOREIGN KEY (`art_author`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
